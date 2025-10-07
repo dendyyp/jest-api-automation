e649070e33bd44524574f13b94d3b12a51bec030
dendyp <dendy@machtwatch.co.id>
2025-10-07 15:35:40 +0700

    API automation with Jest

---
+++ api-test/apitest.test.js
@@ -0,0 +1,35 @@
+var supertest = require('supertest')
+const request = supertest('https://reqres.in')
+
+describe('Get Users', () => {
+    it('should return users data', async () => {
+        const response = await request
+            .get('/api/users?page')
+            .set('x-api-key', 'reqres-free-v1')
+            .set('Content-Type', 'application/json')
+            .expect(200)
+
+        console.log(response.body);
+        expect(Array.isArray(response.body.data)).toBe(true);
+    });
+});
+
+describe('Post New Users', () => {
+    it('should success register new users', async () => {
+        const body = {
+            "name": 'Dendy',
+            "job": 'QA Engineer'
+        };
+
+        const response = await request
+            .post('/api/users')
+            .set('x-api-key', 'reqres-free-v1')
+            .set('Content-Type', 'application/json')
+            .send(body)
+            .expect(201);
+
+        console.log(response.body);
+        expect(response.body).toHaveProperty('name', 'Dendy');
+        expect(response.body).toHaveProperty('job', 'QA Engineer');
+    })
+})
