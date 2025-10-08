0be98e8663c76b367f55e897f4a84d3e96a3a992
dendyp <dendy@machtwatch.co.id>
2025-10-08 14:55:57 +0700

    add scenario for specific users

--- api-test/apitest.test.js
+++ api-test/apitest.test.js
@@ -42,3 +42,28 @@ describe('Post New Users', () => {
         expect(response.body).toHaveProperty('job', 'QA Engineer');
     })
 })
+
+describe('Get specific users', () => {
+    it('should return information for specific users', async () => {
+        const response = await request
+            .get('/api/users/1') //cannot using dynamic id
+            .set('Content-Type', 'application/json')
+            .set(headers)
+            .expect(200);
+
+        console.log(response.body);
+
+        const body = response.body;
+        expect(body).toEqual(
+            expect.objectContaining({
+                data: expect.objectContaining({
+                    id: expect.any(Number),
+                    email: expect.any(String),
+                    first_name: expect.any(String),
+                    last_name: expect.any(String)
+                }),
+            })
+        );
+        console.log('Data structure PASSED');
+    });
+});
